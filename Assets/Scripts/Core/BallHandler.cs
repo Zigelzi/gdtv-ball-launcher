b151dc9b8aaf30e42acbdff9ea74c951afe4c06b
Miika Savela <miika.a.savela@gmail.com>
2022-11-02 07:14:16 +0200

    Add controlling the ball with touch input

---
+++ Assets/Scripts/Core/BallHandler.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class BallHandler : MonoBehaviour
+{
+    GameObject ball;
+
+    void Awake()
+    {
+        ball = GameObject.FindGameObjectWithTag("Ball");
+    }
+    void Update()
+    {
+        HandleTouchInput();
+    }
+
+    void HandleTouchInput()
+    {
+        if (Touchscreen.current.primaryTouch.press.isPressed)
+        {
+            Vector2 touchPosition = Touchscreen.current.primaryTouch.position.ReadValue();
+            Vector2 targetPosition = Camera.main.ScreenToWorldPoint(touchPosition);
+            ball.transform.position = targetPosition;
+        }
+    }
+}
