ec20afd5ce5cb3eebb1cb927c8e0312d82df7955
Miika Savela <miika.a.savela@gmail.com>
2022-11-16 17:22:42 +0200

    Add button to continue to next level

--- Assets/Scripts/UI/Button.cs
+++ Assets/Scripts/UI/Button.cs
@@ -5,6 +5,19 @@ using UnityEngine.SceneManagement;
 
 public class Button : MonoBehaviour
 {
+    [SerializeField] bool _nextLevelButton = false;
+    int _nextLevelIndex = -1;
+
+    void Awake()
+    {
+        _nextLevelIndex = SceneManager.GetActiveScene().buildIndex + 1;
+        DisableNextLevelButtonOnLastLevel();
+    }
+    public void NextLevel()
+    {
+        if (_nextLevelIndex >= SceneManager.sceneCountInBuildSettings) return;
+        SceneManager.LoadScene(_nextLevelIndex);
+    }
     public void RestartLevel()
     {
         int currentLevelIndex = SceneManager.GetActiveScene().buildIndex;
@@ -14,5 +27,14 @@ public class Button : MonoBehaviour
     public void Exit()
     {
         Application.Quit();
+    }
+
+    void DisableNextLevelButtonOnLastLevel()
+    {
+        Debug.Log(SceneManager.sceneCountInBuildSettings);
+        if (_nextLevelButton && _nextLevelIndex >= SceneManager.sceneCountInBuildSettings)
+        {
+            gameObject.SetActive(false);
+        }
     }
 }
