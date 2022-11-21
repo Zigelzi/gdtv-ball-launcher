2f3950bb91dd8c7c72c909d1ca200a9e654a567f
Miika Savela <miika.a.savela@gmail.com>
2022-11-21 20:01:55 +0200

    Add hit VFX and destroying it after it's finished

---
+++ Assets/Scripts/Environment/CollisionEffect.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace DD.Environment
+{
+    public class CollisionEffect : MonoBehaviour
+    {
+        [SerializeField] GameObject vfxPrefab;
+
+        bool _hasCollided = false;
+
+        void OnCollisionEnter(Collision collision)
+        {
+            if (vfxPrefab == null || _hasCollided) return;
+
+            if (collision.collider.CompareTag("Emotion"))
+            {
+                Instantiate(vfxPrefab, collision.contacts[0].point, Quaternion.identity);
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Environment/DestroyEffectAfterFinished.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace DD.Environment
+{
+    public class DestroyEffectAfterFinished : MonoBehaviour
+    {
+        List<ParticleSystem> _childParticles = new List<ParticleSystem>();
+        List<ParticleSystem> _aliveParticles = new List<ParticleSystem>();
+        bool _hasCollided = false;
+
+        void Awake()
+        {
+            GetChildParticles();
+            StartCoroutine(DestroyFinishedFXWithChildren(0));
+        }
+
+        void GetChildParticles()
+        {
+            ParticleSystem[] particles = transform.GetComponentsInChildren<ParticleSystem>();
+
+            foreach (ParticleSystem particle in particles)
+            {
+                _childParticles.Add(particle);
+                _aliveParticles.Add(particle);
+            }
+        }
+
+        IEnumerator DestroyFinishedFXWithChildren(float waitTime)
+        {
+            while (_aliveParticles.Count > 0)
+            {
+                foreach (ParticleSystem vfx in _childParticles)
+                {
+                    DisableFinishedParticle(vfx);
+                }
+                yield return new WaitForSeconds(waitTime);
+            }
+            Destroy(gameObject);
+        }
+
+        void DisableFinishedParticle(ParticleSystem vfx)
+        {
+            if (!vfx.IsAlive())
+            {
+                vfx.gameObject.SetActive(false);
+                _aliveParticles.Remove(vfx);
+            }
+        }
+    }
+
+}
