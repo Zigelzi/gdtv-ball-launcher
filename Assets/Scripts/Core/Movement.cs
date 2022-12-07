bd080899071868fe2e8e0b6e0b558e43ed0c6113
Miika Savela <miika.a.savela@gmail.com>
2022-12-07 09:37:28 +0200

    Upgrade Unity to 2021.3.15f1 and start on gyroscope based movement

---
+++ Assets/Scripts/Core/Movement.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using Gyroscope = UnityEngine.InputSystem.Gyroscope;
+
+namespace DD.Core
+{
+    public class Movement : MonoBehaviour
+    {
+        [SerializeField] float movementSpeed = 20f;
+
+        Rigidbody2D _rb;
+        LinearAccelerationSensor accelerationSensor;
+        UnityEngine.InputSystem.Gyroscope gyro;
+
+        void Awake()
+        {
+            accelerationSensor = LinearAccelerationSensor.current;
+            gyro = Gyroscope.current;
+
+            _rb = GetComponent<Rigidbody2D>();
+        }
+
+        void OnEnable()
+        {
+            InputSystem.EnableDevice(accelerationSensor);
+            InputSystem.EnableDevice(gyro);
+        }
+
+        void Update()
+        {
+            MoveEmotion();
+        }
+
+        void OnDisable()
+        {
+            InputSystem.DisableDevice(accelerationSensor);
+            InputSystem.EnableDevice(gyro);
+        }
+
+        void MoveEmotion()
+        {
+            if (accelerationSensor == null) return;
+
+            Vector2 acceleration = accelerationSensor.acceleration.ReadValue() * movementSpeed;
+
+            _rb.AddForce(acceleration, ForceMode2D.Impulse);
+        }
+    }
+}
---
+++ Assets/Scripts/UI/SensorDisplay.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using Gyroscope = UnityEngine.InputSystem.Gyroscope;
+
+namespace DD.UI
+{
+    public class SensorDisplay : MonoBehaviour
+    {
+        [SerializeField] TMP_Text _textAcceleration;
+        [SerializeField] TMP_Text _textGyro;
+
+        void OnEnable()
+        {
+            if (IsSensorsSupported())
+            {
+                InputSystem.EnableDevice(Gyroscope.current);
+            }
+        }
+
+        void OnDisable()
+        {
+            if(IsSensorsSupported())
+            {
+                InputSystem.DisableDevice(Gyroscope.current);
+            }
+            
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            string acceleration;
+            string gyroAcceleration;
+
+            if (IsSensorsSupported())
+            {
+                gyroAcceleration = Gyroscope.current.angularVelocity.ReadValue().ToString();
+                _textGyro.text = gyroAcceleration;
+            }
+            else
+            {
+                _textAcceleration.text = "Sensors not supported";
+                _textGyro.text = "Sensors not supported";
+            }
+        }
+
+        bool IsSensorsSupported()
+        {
+            if (SystemInfo.supportsGyroscope) return true;
+
+            return false;
+        }
+    }
+}
