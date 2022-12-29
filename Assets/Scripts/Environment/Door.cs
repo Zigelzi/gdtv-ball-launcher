a94493a449b8704f92fb51b18228f1512f96a181
Miika Savela <miika.a.savela@gmail.com>
2022-12-29 18:27:22 +0200

    Add automatically closing the door and preventing triggering opening action while door is moving

--- Assets/Scripts/Environment/Door.cs
+++ Assets/Scripts/Environment/Door.cs
@@ -8,8 +8,10 @@ namespace DD.Environment
     {
         [SerializeField] Transform _targetPosition;
         [SerializeField] float _openingSpeed = 1f;
+        [SerializeField] float _closingSpeed = 1f;
 
         Vector2 _startingPosition;
+        bool _isMoving = false;
         void Awake()
         {
             _startingPosition = transform.position;
@@ -17,17 +19,30 @@ namespace DD.Environment
 
         public void Open()
         {
-            StartCoroutine(MoveDoorToTargetPosition());
+            if (!_isMoving)
+            {
+                StartCoroutine(OpenAndCloseDoor());
+            }
         }
 
-        IEnumerator MoveDoorToTargetPosition()
+        IEnumerator OpenAndCloseDoor()
         {
+            float step = _openingSpeed * Time.deltaTime;
+
+            _isMoving = true;
+
             while (Vector2.Distance(_targetPosition.position, transform.position) > 0.1f)
             {
-                float step = _openingSpeed * Time.deltaTime;
                 transform.position = Vector2.MoveTowards(transform.position, _targetPosition.position, step);
                 yield return new WaitForEndOfFrame();
             }
+            while (Vector2.Distance(_startingPosition, transform.position) > 0.1f)
+            {
+                transform.position = Vector2.MoveTowards(transform.position, _startingPosition, step);
+                yield return new WaitForEndOfFrame();
+            }
+
+            _isMoving = false;
         }
     }
 }
